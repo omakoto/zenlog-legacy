c7374eb6f2a52ad4d7a04352889e091ef0048b04
Makoto Onuki <makoto.onuki@gmail.com>
2017-03-21 23:03:25 -0700

    v43, script working.

--- ruby/test.rb
+++ ruby/test.rb
@@ -2,25 +2,11 @@
 
 DEBUG = true;
 
-# f = File.open('/tmp/a', 'w')
-# f.close_on_exec = false
-# exec 'ls', '-l', '/proc/self/fd', close_others: false
-# exit 0
-
-
-# f = File.open("/tmp/abc", "w");
-# f.close_on_exec = false;
-# f.write("test\n");
-# system "ls", "-l", "/proc/self/fd";
-
 reader, writer = IO.pipe;
 writer.close_on_exec = false;  # STOPSHIP Figure out why it won't work.
 printf "Reader=%d Writer=%d\n", reader, writer if DEBUG;
-# system "ls", "-l", "/proc/self/fd" if DEBUG;
-# writer.write "abc";
-# system "ls", "-l", "/proc/self/fd" if DEBUG;
 
-fork do
+child_pid = fork do
   puts ">> In child." if DEBUG;
   exec "ls", "-l", "/proc/self/fd", close_others: false;
   exit 0;
@@ -28,4 +14,5 @@ end
 puts ">> In parent." if DEBUG;
 writer.close
 
+Process.waitpid child_pid
 exit 0;
