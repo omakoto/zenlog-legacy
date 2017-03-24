3f043eeaabc88fc3433be362d0c5ff9a713ed43b
Makoto Onuki <omakoto@google.com>
2017-03-23 17:12:07 -0700

    update comment

--- v2/dot_zenlogrc.pl
+++ v2/dot_zenlogrc.pl
@@ -1,4 +1,4 @@
-# Zenlog initialization file.
+# Sample zenlog initialization file.
 
 # Start this command instead of the default shell.
 $ENV{ZENLOG_START_COMMAND} = "$ENV{HOME}/cbin/bin-linux64/lbash -l";
@@ -11,4 +11,4 @@ $ENV{ZENLOG_DIR} = "/zenlog/";
 $ENV{ZENLOG_PREFIX_COMMANDS} = "(?:builtin|time|ee|eet|wb|Test\:|Running\:?|forever|FI|sudo)";
 
 # Always do not log output from these commands.
-$ENV{ZENLOG_ALWAYS_184_COMMANDS} = "(?:cd|vi|vim|man|nano|pico|less|watch|emacs|mhist|root|ssh|ssh-ce|ssh-com|htop|nload)";
+$ENV{ZENLOG_ALWAYS_184_COMMANDS} = "(?:vi|vim|man|nano|pico|less|watch|emacs|mhist|root|ssh|ssh-ce|ssh-com|htop|nload)";
