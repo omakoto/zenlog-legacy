0cbcf67a0cda5b4a7c11bd7c93fd6857b4c75c3e
Makoto Onuki <makoto.onuki@gmail.com>
2017-10-21 20:48:37 -0700

    Fixed command name detection + more tests.

--- v4/zenlog-defaults.rb
+++ v4/zenlog-defaults.rb
@@ -1,4 +1,4 @@
 DEFAULT_ZENLOG_DIR = Dir.home() + "/zenlog"
 DEFAULT_ZENLOG_START_COMMAND = "#{ENV['SHELL']} -l"
-DEFAULT_ZENLOG_PREFIX_COMMANDS = "(?:command|builtin|time|sudo)"
+DEFAULT_ZENLOG_PREFIX_COMMANDS = "(?:command|builtin|time|sudo|[a-zA-Z0-9_]+\=.*)"
 DEFAULT_ZENLOG_ALWAYS_NO_LOG_COMMANDS = "(?:vi|vim|man|nano|pico|less|watch|emacs|zenlog.*)"
